fa166bf5938c512c776c5f8bdb4a3a06f1cf1279
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2022-05-24 12:12:40 -0300

    Adds serverless v0

---
+++ poc/v0/main.tf
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ locals {
+  serverless_apis = ["vpcaccess.googleapis.com", "compute.googleapis.com", "container.googleapis.com", "run.googleapis.com", "cloudkms.googleapis.com"]
+  vpc_apis        = ["vpcaccess.googleapis.com", "container.googleapis.com"]
+}
+
+resource "google_project_service" "serverless_project_apis" {
+  for_each           = toset(local.serverless_apis)
+  project            = var.serverless_project
+  service            = each.value
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "vpc_project_apis" {
+  for_each           = toset(local.vpc_apis)
+  project            = var.vpc_project
+  service            = each.value
+  disable_on_destroy = false
+}
+
+ resource "google_project_service_identity" "serverless_sa" {
+  provider = google-beta
+  project  = var.serverless_project
+  service  = "run.googleapis.com"
+}
+
+data "google_project" "serveeless_project" {
+    project_id = var.serverless_project
+}
+
+module "cloud_run" {
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "~> 0.3.0"
+
+  service_name          = "hello-world-with-apis-test"
+  project_id            = var.serverless_project
+  location              = var.location
+  image                 = var.image
+  service_account_email = var.cloud_run_sa
+  template_annotations = {
+    "autoscaling.knative.dev/maxScale" : 2,
+    "autoscaling.knative.dev/minScale" : 1,
+    "run.googleapis.com/vpc-access-connector" = tolist(module.serverless-connector.connector_ids)[0],
+    "run.googleapis.com/vpc-access-egress"    = "all-traffic"
+  }
+
+  encryption_key = module.cloud_run_kms.keys["cloud_run"]
+
+  env_vars = [{ name : "PROJECT_ID", value : var.serverless_project }]
+
+  depends_on = [
+    module.serverless-connector
+  ]
+}
---
+++ poc/v0/variables.tf
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "terraform_service_account" {
+  description = "The email address of the service account that will run the Terraform code."
+  type        = string
+}
+
+variable "location" {
+  description = "The location where resources are going to be deployed."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "vpc_project" {
+  description = "The project where shared vpc is."
+  type        = string
+}
+
+variable "kms_project" {
+  description = "The project where KMS will be created."
+  type        = string
+}
+
+variable "serverless_project" {
+  description = "The project where cloud run is going to be deployed."
+  type        = string
+}
+
+variable "shared_vpc_name" {
+  description = "Shared VPC name."
+  type        = string
+}
+
+variable "image" {
+  description = "Image url to be deployed on Cloud Run."
+  type        = string
+}
+
+variable "artifact_repository_project" {
+  description = "Artifact Repository Project to grant serverless identity viewer role."
+  type        = string
+}
+
+variable "artifact_repository_location" {
+  description = "Artifact Repository location to grant serverless identity viewer role."
+  type        = string
+}
+
+variable "artifact_repository_name" {
+  description = "Artifact Repository name to grant serverless identity viewer role"
+  type        = string
+}
+
+variable "cloud_run_sa" {
+    description = "Service account to be used on Cloud Run."
+    type = string
+}
