663933dfc187a1588b1c9b61ab97cb549d7973de
Samir-Cit <samir@ciandt.com>
2022-06-01 16:18:41 -0300

    Adding new rules for Cloud Armor.

--- poc/v0/cloud_armor_policy.tf
+++ poc/v0/cloud_armor_policy.tf
@@ -23,8 +23,48 @@ variable "default_rules" {
      default = {
          rule_sqli = {
              action = "deny(403)"
+             priority = "1000"
+             expression = "evaluatePreconfiguredExpr('sqli-stable', ['owasp-crs-v030001-id942251-sqli', 'owasp-crs-v030001-id942420-sqli', 'owasp-crs-v030001-id942431-sqli', 'owasp-crs-v030001-id942460-sqli', 'owasp-crs-v030001-id942421-sqli', 'owasp-crs-v030001-id942432-sqli'])"
+         }
+         rule_xss = {
+             action = "deny(403)"
+             priority = "1001"
+             expression = "evaluatePreconfiguredExpr('xss-stable', ['owasp-crs-v030001-id941150-xss', 'owasp-crs-v030001-id941320-xss', 'owasp-crs-v030001-id941330-xss'])" #There's more to be added
+         }
+         rule_lfi = {
+             action = "deny(403)"
              priority = "1002"
              expression = "evaluatePreconfiguredExpr('lfi-stable')"
+         }
+         rule_canary = {
+             action = "deny(403)"
+             priority = "1003"
+             expression = "evaluatePreconfiguredExpr('rce-stable')"
+         }
+         rule_rfi = {
+             action = "deny(403)"
+             priority = "1004"
+             expression = "evaluatePreconfiguredExpr('rfi-stable', ['owasp-crs-v030001-id931130-rfi'])"
+         }
+         rule_methodenforcement = {
+             action = "deny(403)"
+             priority = "1005"
+             expression = "evaluatePreconfiguredExpr('methodenforcement-stable')"
+         }
+         rule_scandetection = {
+             action = "deny(403)"
+             priority = "1006"
+             expression = "evaluatePreconfiguredExpr('scandetection-stable', ['owasp-crs-v030001-id913101-scandetection', 'owasp-crs-v030001-id913102-scandetection'])"
+         }
+         rule_protocolattach = {
+             action = "deny(403)"
+             priority = "1007"
+             expression = "evaluatePreconfiguredExpr('protocolattack-stable', ['owasp-crs-v030001-id921151-protocolattack', 'owasp-crs-v030001-id921170-protocolattack'])"
+         }
+         rule_sessionfixation = {
+             action = "deny(403)"
+             priority = "1009"
+             expression = "evaluatePreconfiguredExpr('sessionfixation-stable')"
          }
      }
      type = map(object({
