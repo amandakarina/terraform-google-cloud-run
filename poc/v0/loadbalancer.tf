990ea17be34a89a0ddff917ed52c4619c19d9081
Renato Rudnicki <renatojr@ciandt.com>
2022-06-06 17:58:15 -0300

    adding cloud armor rules

--- poc/v0/loadbalancer.tf
+++ poc/v0/loadbalancer.tf
@@ -63,5 +63,33 @@ resource "google_compute_region_network_endpoint_group" "serverless_neg" {
 resource "google_compute_security_policy" "cloud-armor-security-policy" {
     project = var.serverless_project_id
     name = "cloud-armor-waf-policy"
+    
+    dynamic "rule" {
+        for_each = var.default_rules
+        content {
+            action = rule.value.action
+            priority = rule.value.priority
+            description = rule.value.description
+            match {
+                versioned_expr = rule.value.versioned_expr
+                config {
+                    src_ip_ranges = rule.value.src_ip_ranges
+                }
+            }
+        }
+    }
+
+     dynamic "rule" {
+         for_each = var.owasp_rules
+         content {
+             action = rule.value.action
+             priority = rule.value.priority
+             match {
+                 expr {
+                     expression = rule.value.expression
+                 }
+             }
+         }
+     }
 }
 
