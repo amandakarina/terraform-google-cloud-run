ee703a4bd575828fea0d3699370f15018d258098
Samir-Cit <samir@ciandt.com>
2022-11-18 14:51:44 -0300

    CR fix.

--- examples/secure_cloud_run_standalone/main.tf
+++ examples/secure_cloud_run_standalone/main.tf
@@ -88,20 +88,6 @@ module "secure_cloud_run" {
     ssl_certificates_self_links       = []
   }
 
-  # If you are going to use secrets as volume uncomment this part of the code and fill with your values.
-  # Also, to use secrets from another project you will need to create a VPC directional rule or add the project to the peremiter.
-  # volumes = [
-  #   {
-  #     name = "VOLUME_NAME",
-  #     secret = [
-  #       {
-  #         secret_name = "SECRET_NAME",
-  #         items       = { key = "SECRET_VERSION", "path" = "projects/PROJECT_NUMBER/secrets/" }
-  #       }
-  #     ]
-  #   }
-  # ]
-
   depends_on = [
     null_resource.copy_image
   ]
